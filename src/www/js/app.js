bb29ec69e1290d3402681755151709d4a4bc3404
Merrick Zhang <anphorea@gmail.com>
2013-12-09 23:19:10 +0800

    ALPHA 1

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -34,10 +34,117 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        app.checkPlugin();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         console.log('Received Event: ' + id);
-        one.getCopyRightInfo();
+        app.bbuistart();
+    },
+    checkPlugin:function(){
+	    if (community && community.curl){
+		    console.log('community.curl loaded.');
+	    }else{
+		    console.log('community.curl not found.');
+		    window.alert('community.curl not found.');
+	    }
+    },
+    bbuistart:function(){
+	    // This is code to ensure that if webworksready is fired multiple times we still only init() one time
+		if (webworksreadyFired) return;
+		webworksreadyFired = true;
+		var config;
+
+		// Toggle our coloring for testing 
+		if (darkColoring) {
+			config = {
+				controlsDark: true,
+				listsDark: true
+			};
+		} else {
+			config = {
+				controlsDark: false,
+				listsDark: false,
+				coloredTitleBar: true
+			};
+		}
+
+		// Handle styling of the screen before it is displayed
+		config.onscreenready = function(element, id) {
+			if (darkColoring) {
+				var screen = element.querySelector('[data-bb-type=screen]');
+				if (screen) {
+					screen.style['background-color'] = darkScreenColor;
+				}
+			}
+		};
+
+		// Handle styling of the screen after it is displayed
+		config.ondomready = function(element, id, params) {
+			if (id == 'menu') {
+				loadContent(element, id);
+			}
+		};
+
+		bb.init(config);
+		if (darkColoring) {
+			document.body.style['background-color'] = darkScreenColor;
+			document.body.style['color'] = 'white';
+		}
+		bb.pushScreen('main.html', 'menu');
     }
 };
+
+function getJSON(URL){
+	//URL should be started with "http://"
+	return JSON.parse(community.curl.get(URL));
+}
+function loadContent(element, id){
+	var d=new Date();
+	var strdate=d.format('yyyy-MM-dd');
+
+	setTimeout(loadpic(element,strdate),1);
+
+}
+var _pic,_one,_ask;
+function loadpic(element,strdate){
+	var data=one.getHomePage(strdate)["hpEntity"];
+	_pic="<div><a name='pic'></a><img src='"+data['strOriginalImgUrl']+"' width=100% ><h3>"+data["strHpTitle"]+" -- "+data['strAuthor']+"</h3> <h3>"+data['strContent']+"</h3></div>";
+	var content=one.getOneContentInfo(strdate)["contentEntity"];
+	_one="<div><a name='one'></a><h3>"+content["strContTitle"]+"</h3><h3><i>"+content["strContAuthorIntroduce"]+"</i> "+content["strContAuthor"]+"</h3><div>"+content["strContent"]+"</div></div>";
+	var ask=one.getOneQuestionInfo(strdate)['questionAdEntity'];
+	_ask="<div><a name='ask'></a><h3>"+ask['strQuestionTitle']+"</h3><div>"+ask['strQuestionContent']+"</div><h3>"+ask['strAnswerTitle']+"</h3><div>"+ask["strAnswerContent"]+"</div></div>";
+		
+	element.getElementById('appcontent').innerHTML=_pic+_one+_ask;
+}
+
+Date.prototype.format = function(format)
+{
+ var o = {
+ "M+" : this.getMonth()+1, //month
+ "d+" : this.getDate(),    //day
+ "h+" : this.getHours(),   //hour
+ "m+" : this.getMinutes(), //minute
+ "s+" : this.getSeconds(), //second
+ "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+ "S" : this.getMilliseconds() //millisecond
+ }
+ if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+ (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+ for(var k in o)if(new RegExp("("+ k +")").test(format))
+ format = format.replace(RegExp.$1,
+ RegExp.$1.length==1 ? o[k] :
+ ("00"+ o[k]).substr((""+ o[k]).length));
+ return format;
+}
+
+function showTab(id) {
+	// switch between tabs.
+	if (id == 'tab_pic') {
+		document.location.hash="#pic";
+	} else if (id == 'tab_one') {
+		document.location.hash="#one";
+	} else {
+		document.location.hash="#ask";
+	}
+}
--- src/www/js/one.js
+++ src/www/js/one.js
@@ -1,20 +1,185 @@
-var one={
-	
-};
-
-function getJSON(URL,callback){
-	$.ajax({
-		url:URL,
-    	type:'GET',
-    	callback: 'CALLBACK',
-		dataType:"JSON",
-		data:{},
-		success:function(resp){
-			console.log(resp);
+﻿var one = {
+	getCopyRightInfo: function() {
+		var cp = getJSON('http://211.152.49.184:7001/OneForWeb/onest/getCrInf');
+		return cp["entCr"]["strCR"];
+	},
+	getHpAdMultiinfo: function() {
+		//获取5天的广告与首页数据
+		/*
+		data:
+		{
+		    "result": "SUCCESS/FAIL",
+		    "message": "Description",
+		    "hpAdMulitEntity": {
+		        "lstEntAd": [
+		            {
+		                "strAd1280720url": "http://pic.yupoo.com/hanapp/CWfa9OMH/w4NHA.jpg",
+		                "strAd480320url": "http://pic.yupoo.com/hanapp/CWfa9OMH/w4NHA.jpg",
+		                "strAd800480url": "http://pic.yupoo.com/hanapp/CWfa9OMH/w4NHA.jpg",
+		                "strAd960540url": "http://pic.yupoo.com/hanapp/CWfa9OMH/w4NHA.jpg",
+		                "strAdFutureurl": "http://pic.yupoo.com/hanapp/CWfa9OMH/w4NHA.jpg",
+		                "strAdId": "21",
+		                "strAdLinkUrl": "http://www.sportq.com/download/sportq.apk",
+		                "strAdOrder": "1",
+		                "strCloseTime": "2013-08-31",
+		                "strMarketTime": "2013-08-01",
+		                "strType": "2"
+		            }
+		        ],
+		        "lstEntHp": [
+		            {
+		                "sWebLk": "http://hanhan.qq.com/hanhan/one/one314m.htm",
+		                "strAuthor": "港湾&摄影/马德里小糖饼",
+		                "strContent": "浪荡天涯的孩子，忽晴忽雨的江湖，祝你有梦为马，永远随处可栖。by 大冰",
+		                "strDayDiffer": "1",
+		                "strHpId": "330",
+		                "strHpTitle": "VOL.314",
+		                "strLastUpdateDate": "2013-08-15 15:41:21",
+		                "strMarketTime": "2013-08-17",
+		                "strOriginalImgUrl": "http://pic.yupoo.com/hanapp/D5dAKZtU/oy6O7.jpg",
+		                "strThumbnailUrl": "http://pic.yupoo.com/hanapp/D5dAKZtU/oy6O7.jpg"
+		            }
+		        ]
+		    }
 		}
-	})
-}
-one.getCopyRightInfo=function(){
-	getJSON('http://211.152.49.184:7001/OneForWeb/onest/getCrInf',function(data){
-	})
-}
+		*/
+		var cp = getJSON('http://211.152.49.184:7001/OneForWeb/one/getHpAdMultiinfo');
+		return cp;
+	},
+	getLateds30dayPraiseNumber: function() {
+		/*
+		{
+			"result":"SUCCESS",
+			"lstContentPraise":[
+				{"strContentId":"492",
+				"strPraiseNumber":"6737"},
+				{"strContentId":"491",
+				"strPraiseNumber":"28389"},
+				{"strContentId":"490",
+				"strPraiseNumber":"22234"},
+				{"strContentId":"487",
+				"strPraiseNumber":"5037"},
+				{"strContentId":"486",
+				"strPraiseNumber":"9914"},
+				{"strContentId":"485",
+				"strPraiseNumber":"15558"},
+				{"strContentId":"484",
+				"strPraiseNumber":"14138"},
+				{"strContentId":"483",
+				"strPraiseNumber":"23276"},
+				{"strContentId":"482",
+				"strPraiseNumber":"13170"},
+				{"strContentId":"480",
+				"strPraiseNumber":"36382"}
+				]
+		}
+		*/
+		var cp = getJSON('http://211.152.49.184:7001/OneForWeb/one/getLateds30dayPraiseNumber');
+		return cp;
+	},
+	getMobileDispDays: function() {
+		/*
+		{
+			"result":"SUCCESS",
+			"mobileDispCtrlDays":"10"
+		}
+		*/
+		var cp = getJSON('http://211.152.49.184:7001/OneForWeb/one/getMobileDispDays');
+		return cp;
+	},
+	getTPage: function() {
+		/*
+		{
+			"result":"SUCCESS",
+			"entAPAndPos":
+				{
+					"lstConPos":["11"],
+					"lstHpPos":["11"],
+					"lstQuePos":["11"],
+					"sConNum":"10",
+					"sHpNum":"10",
+					"sQNum":"10"
+				}
+		}
+		*/
+		var cp = getJSON('http://211.152.49.184:7001/OneForWeb/one/getTPage');
+		return cp;
+	},
+	getHomePage: function(datestr) {
+		/*
+		data:
+		{
+		    "result": "SUCCESS/FAIL",
+		    "message": "Description",
+		    "hpEntity": {
+		        "sWebLk": "http://hanhan.qq.com/hanhan/one/one314m.htm",
+		        "strLastUpdateDate": "2013-08-15 15:41:21",
+		        "strMarketTime": "2013-08-17",
+		        "strDayDiffer": "1",
+		        "strHpId": "330",
+		        "strHpTitle": "VOL.314",
+		        "strAuthor": "港湾&摄影/马德里小糖饼",
+		        "strContent": "浪荡天涯的孩子，忽晴忽雨的江湖，祝你有梦为马，永远随处可栖。by 大冰",
+		        "strOriginalImgUrl": "http://pic.yupoo.com/hanapp/D5dAKZtU/oy6O7.jpg",
+		        "strThumbnailUrl": "http://pic.yupoo.com/hanapp/D5dAKZtU/oy6O7.jpg"
+		    }
+		}
+		*/
+		var cp = getJSON('http://211.152.49.184:7001/OneForWeb/one/getHpinfo?strDate=' + datestr)
+		return cp;
+	},
+	getOneContentInfo: function(datestr) {
+		/*
+		data:
+		{
+		    "result": "SUCCESS/FAIL",
+		    "message": "Description",
+		    "contentEntity": {
+		        "sGW": "财色名食睡...",
+		        "sWebLk": "http://hanhan.qq.com/hanhan/one/one314m.htm#page1",
+		        "strLastUpdateDate": "2013-08-16 23:07:26",
+		        "strPraiseNumber": "4463",
+		        "strContAuthor": "大冰",
+		        "strContAuthorIntroduce": "（责任编辑：赵西栋）",
+		        "strContDayDiffer": "1",
+		        "strContMarketTime": "2013-08-17",
+		        "strContentId": "344",
+		        "strContTitle": "他们最幸福",
+		        "strContent": "
+		这是篇演讲稿...
+		\r\n
+		大冰，主持人、民谣歌手；微博ID：@大冰<\/b><\/div>"
+		    }
+		}
+		*/
+		var cp = getJSON('http://211.152.49.184:7001/OneForWeb/one/getOneContentInfo?strDate=' + datestr);
+		return cp;
+	},
+	getOneQuestionInfo: function(datestr) {
+		/*
+		data:
+		{
+		    "result": "SUCCESS/FAIL",
+		    "message": "Description",
+		    "questionAdEntity": {
+		        "sWebLk": "http://hanhan.qq.com/hanhan/one/one314m.htm#page2",
+		        "strDayDiffer": "1",
+		        "strLastUpdateDate": "2013-08-17 09:30:10",
+		        "strPraiseNumber": "2074",
+		        "strQuestionId": "325",
+		        "strQuestionMarketTime": "2013-08-17",
+		        "strQuestionTitle": "你想象中的天堂是什么样子的？"
+		        "strQuestionContent": "「一个」工作室问微信网友：有人把天堂想象成是一座图书馆...",
+		        "strAnswerTitle": "微信网友答「一个」工作室：",
+		        "strAnswerContent": "
+		Ym：雨从云里落下来...
+		\r\n...
+		（责任编辑：赵西栋）
+		",
+		    }
+		}
+		*/
+		var cp = getJSON('http://211.152.49.184:7001/OneForWeb/one/getOneQuestionInfo?strDate=' + datestr);
+		return cp;
+	}
+};
