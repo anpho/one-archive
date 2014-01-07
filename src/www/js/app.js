d37746c70c343367cd1c639023c15d0fedc6b908
Merrick Zhang <anphorea@gmail.com>
2014-01-07 12:28:38 +0800

    新布局的代码库，FOR 0.1.1

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -70,15 +70,14 @@ var app = {
 					screen.style['background-color'] = darkScreenColor;
 				}
 			}
-			if (id == 'menu') {
-				if (community)loadContent(element, id);
-				//loadcp(element,id);
-			}
+
 		};
 
 		// Handle styling of the screen after it is displayed
 		config.ondomready = function(element, id, params) {
-			setTimeout(showTab('home'), 10);
+			if (id == 'menu') {
+				loadContent(element, id);
+			}
 		};
 
 		bb.init(config);
@@ -92,10 +91,10 @@ var app = {
 
 function getJSON(URL) {
 	//URL should be started with "http://"
-	try{
-	return JSON.parse(community.curl.get(URL));
-	}catch(e){
-		window.alert('与服务器联系出错');
+	try {
+		return JSON.parse(community.curl.get(URL));
+	} catch (e) {
+		window.alert('Can not connect to server.\n This app is only available in China.');
 		location.reload(true);
 	}
 }
@@ -104,6 +103,7 @@ function loadContent(element, id) {
 	var d = new Date();
 	var strdate = d.format('yyyy-MM-dd');
 	loadAll(element, strdate);
+	showTab('home');
 }
 
 function g(id) {
@@ -117,84 +117,78 @@ function gg(doc, id) {
 function loadAll(element, strdate) {
 	currentdisplaydate = strdate;
 	loadhome(element, strdate);
-	loadOne(element, strdate);
-	loadQuestion(element, strdate);
+	setTimeout(function(){
+		loadOne(element, strdate);
+		}, 100);
+	setTimeout(function(){
+		loadQuestion(element, strdate);
+		}, 100);
+	
 }
 
 function loadhome(element, strdate) {
-	var starttime = new Date().valueOf();
-	console.log('load-home');
 	var data;
 	if (localStorage[strdate + 'home']) {
 		data = JSON.parse(localStorage[strdate + 'home']);
 	} else {
 		data = one.getHomePage(strdate)["hpEntity"];
-		localStorage[strdate + 'home'] = JSON.stringify(data);
+		setTimeout(function() {
+			localStorage[strdate + 'home'] = JSON.stringify(data);
+		}, 100);
 	}
-	console.log('home Load Object: ' + (new Date().valueOf() - starttime));
-	starttime = new Date().valueOf();
 	gg(element, 'home-title').innerHTML = strdate;
 	gg(element, 'home-img').src = data['strOriginalImgUrl'];
 	gg(element, 'home-img').lowsrc = data['strThumbnailUrl'];
 	gg(element, 'home-vol').innerHTML = data['strHpTitle'];
 	gg(element, 'home-img-by').innerHTML = data['strAuthor'].replace(/&/g, '<br/>');
 	gg(element, 'home-content').innerHTML = data['strContent'];
-	console.log('home Set DOM:' + (new Date().valueOf() - starttime));
+	console.log('Home Loaded.')
 }
 
 function loadOne(e, strdate) {
-	var starttime = new Date().valueOf();
 	var content;
 	if (localStorage[strdate + 'one']) {
 		content = JSON.parse(localStorage[strdate + 'one']);
 	} else {
 		content = one.getOneContentInfo(strdate)["contentEntity"];
-		localStorage[strdate + 'one'] = JSON.stringify(content);
+		setTimeout(function() {
+			localStorage[strdate + 'one'] = JSON.stringify(content);
+		}, 100);
 	}
-	console.log('loadOne Load Object: ' + (new Date().valueOf() - starttime));
-	starttime = new Date().valueOf();
 	gg(e, 'c-brief').innerHTML = content['sGW'];
 	gg(e, 'c-title').innerHTML = content['strContTitle'];
 	gg(e, 'c-author-intro').innerHTML = content['strContAuthorIntroduce'];
 	gg(e, 'c-author').innerHTML = content['strContAuthor'];
 	gg(e, 'c-content').innerHTML = "<p>" + content['strContent'].replace(/<br>/g, "</p><p>") + "</p>";
-	console.log('loadOne Set DOM:' + (new Date().valueOf() - starttime));
+	console.log('Content Loaded.');
 }
 
 function loadQuestion(e, strdate) {
-	var starttime = new Date().valueOf();
 	var ask;
 	if (localStorage[strdate + 'ask']) {
 		ask = JSON.parse(localStorage[strdate + 'ask']);
 	} else {
 		ask = one.getOneQuestionInfo(strdate)['questionAdEntity'];
-		localStorage[strdate + 'ask'] = JSON.stringify(ask);
+		setTimeout(function() {
+			localStorage[strdate + 'ask'] = JSON.stringify(ask);
+		}, 1000);
 	}
-	console.log('loadQuestion Load Object: ' + (new Date().valueOf() - starttime));
-	starttime = new Date().valueOf();
-
 	gg(e, 'q-title').innerHTML = ask['strQuestionTitle'];
 	gg(e, 'q-content').innerHTML = ask['strQuestionContent'];
 	gg(e, 'a-title').innerHTML = ask['strAnswerTitle'];
 	gg(e, 'a-content').innerHTML = ask['strAnswerContent'];
-	console.log('loadQuestion Set DOM:' + (new Date().valueOf() - starttime));
+	console.log('Question Loaded.')
 }
 
 Date.prototype.format = function(format) {
 	var o = {
 		"M+": this.getMonth() + 1,
-		//month
 		"d+": this.getDate(),
-		//day
 		"h+": this.getHours(),
-		//hour
 		"m+": this.getMinutes(),
-		//minute
 		"s+": this.getSeconds(),
-		//second
 		"q+": Math.floor((this.getMonth() + 3) / 3),
-		//quarter
-		"S": this.getMilliseconds() //millisecond
+		"S": this.getMilliseconds()
 	}
 	if (/(y+)/.test(format)) format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
 	for (var k in o) if (new RegExp("(" + k + ")").test(format)) format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
@@ -202,21 +196,22 @@ Date.prototype.format = function(format) {
 }
 
 function showTab(id) {
+	document.location.hash = "";
 	// switch between tabs.
 	if (id == 'home') {
-		g('home').style.display="block";;
-		g('content').style.display="none";
-		g('ask').style.display="none";
+		//g('home').style.display = "block";
+		//g('content').style.display = "none";
+		//g('ask').style.display = "none";
 		document.location.hash = "#h-top";
 	} else if (id == 'content') {
-		g('home').style.display="none";
-		g('content').style.display="block";;
-		g('ask').style.display="none";
+		//g('content').style.display = "block";
+		//g('home').style.display = "none";
+		//g('ask').style.display = "none";
 		document.location.hash = "#c-top";
 	} else if (id == 'question') {
-		g('home').style.display="none";
-		g('content').style.display="none";
-		g('ask').style.display="block";;
+		//g('ask').style.display = "block";
+		//g('home').style.display = "none";
+		//g('content').style.display = "none";
 		document.location.hash = "#q-top";
 	}
 }
